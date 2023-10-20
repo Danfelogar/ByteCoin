eac6ca9fe7c27113d6d09e2fe97b6811f69c185f
Danfelogar <danypolo18@hotmail.com>
2023-10-19 22:25:51 -0500

    Initial Commit

---
+++ ByteCoin/Model/CoinModel.swift
@@ -0,0 +1,18 @@
+//
+//  CoinModel.swift
+//  ByteCoin
+//
+//  Created by Daniel Felipe on 17/09/23.
+//  Copyright © 2023 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+
+struct CoinModel {
+    let time: String
+    let assetIdBase: String
+    let assetIdQuote: String
+    let rate: Double
+    
+}
